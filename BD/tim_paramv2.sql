6a6959dc7a24a20df0e8d96206c4682f91db3ec7
timoteanglade <timote.anglade.45@gmail.com>
2023-05-25 22:09:45 +0200

    update bdd

--- BD/tim_paramv2.sql
+++ BD/tim_paramv2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3307
--- Généré le : mar. 09 mai 2023 à 20:09
+-- Généré le : jeu. 25 mai 2023 à 20:09
 -- Version du serveur : 10.6.11-MariaDB
 -- Version de PHP : 8.0.26
 
@@ -62,11 +62,21 @@ CREATE TABLE IF NOT EXISTS `archivecontenir` (
 DROP TABLE IF EXISTS `avis`;
 CREATE TABLE IF NOT EXISTS `avis` (
   `mail` varchar(50) NOT NULL,
-  `idAvis` int(11) NOT NULL,
-  `note` float NOT NULL,
-  `contenu_avis` int(11) NOT NULL,
-  PRIMARY KEY (`mail`,`idAvis`)
+  `id` varchar(32) NOT NULL,
+  `note` int(1) NOT NULL,
+  `contenu_avis` text NOT NULL,
+  PRIMARY KEY (`mail`,`id`),
+  KEY `avis_produit_fk` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `avis`
+--
+
+INSERT INTO `avis` (`mail`, `id`, `note`, `contenu_avis`) VALUES
+('a.fougerard@gmail.fr', 'c01', 3, 'fgsdfg'),
+('timote.anglade.45@gmail.com', 'c01', 2, 'Très mauvais rapport qualité/prix.'),
+('timote.anglade.45@gmail.com', 'c02', 5, 'C\'est vraiment super cool');
 
 -- --------------------------------------------------------
 
@@ -101,9 +111,19 @@ CREATE TABLE IF NOT EXISTS `commande` (
   `mail` varchar(50) NOT NULL,
   `id` char(32) NOT NULL,
   `dateCommande` date DEFAULT NULL,
-  `etatCde` char(1) NOT NULL,
+  `etatCde` char(1) NOT NULL DEFAULT 'P' COMMENT 'T = Transport\r\nP = en Préparation\r\nA = Arrivée\r\nC = Cancel',
   PRIMARY KEY (`mail`,`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`mail`, `id`, `dateCommande`, `etatCde`) VALUES
+('timote.anglade.45@gmail.com', '1', '2023-04-20', 'T'),
+('timote.anglade.45@gmail.com', '2', '2023-05-25', 'T'),
+('timote.anglade.45@gmail.com', '3', '2023-03-15', 'P'),
+('timote.anglade.45@gmail.com', '4', '2022-10-11', 'P');
 
 -- --------------------------------------------------------
 
@@ -130,22 +150,7 @@ CREATE TABLE IF NOT EXISTS `compte` (
 
 INSERT INTO `compte` (`mail`, `pass`, `nom`, `prenom`, `adresseRue`, `ville`, `cp`, `admin`) VALUES
 ('a.fougerard@gmail.fr', '$2y$10$thuLWMDRfGPnO8St6vwu7OuojH9NbPmguDZO4ISoRvC.Z.pyvwahe', 'Fougerard', 'Antonin', '78 Chépaou', 'Orléans', '45000', 1),
-('timote.anglade.45@gmail.com', '$2y$10$1VKMBjDHcDQ1MPtxpMyvVuyWqGRrkAAuNg5qp21a1EBO28CEXDuhW', 'Anglade Timoté', 'Timoté', '11 Rue Eulalie Lebrun', 'Chateauneuf Sur Loire', '45110', 0);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `concerne`
---
-
-DROP TABLE IF EXISTS `concerne`;
-CREATE TABLE IF NOT EXISTS `concerne` (
-  `id` char(32) NOT NULL,
-  `mail` varchar(50) NOT NULL,
-  `idAvis` int(11) NOT NULL,
-  PRIMARY KEY (`id`,`mail`,`idAvis`),
-  KEY `concerne_avis0_FK` (`mail`,`idAvis`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+('timote.anglade.45@gmail.com', '$2y$10$1VKMBjDHcDQ1MPtxpMyvVuyWqGRrkAAuNg5qp21a1EBO28CEXDuhW', 'Anglade', 'Timoté', '11 Rue Eulalie Lebrun', 'Chateauneuf Sur Loire', '45110', 1);
 
 -- --------------------------------------------------------
 
@@ -160,9 +165,22 @@ CREATE TABLE IF NOT EXISTS `contenir` (
   `mail_commande` varchar(50) NOT NULL,
   `id_commande` char(32) NOT NULL,
   `qte` int(11) NOT NULL DEFAULT 1,
+  `reduction` float DEFAULT NULL,
   PRIMARY KEY (`id_produit`,`id_contenance`,`mail_commande`,`id_commande`),
   KEY `contenir_commande0_FK` (`mail_commande`,`id_commande`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `contenir`
+--
+
+INSERT INTO `contenir` (`id_produit`, `id_contenance`, `mail_commande`, `id_commande`, `qte`, `reduction`) VALUES
+('c01', 1, 'timote.anglade.45@gmail.com', '1', 1, 0.5),
+('c03', 1, 'timote.anglade.45@gmail.com', '2', 1, 1),
+('c07', 1, 'timote.anglade.45@gmail.com', '3', 16, 1),
+('f02', 1, 'timote.anglade.45@gmail.com', '4', 1, 0.5),
+('p02', 1, 'timote.anglade.45@gmail.com', '3', 11, 1),
+('p06', 1, 'timote.anglade.45@gmail.com', '3', 6, 0.5);
 
 -- --------------------------------------------------------
 
@@ -214,15 +232,6 @@ CREATE TABLE IF NOT EXISTS `panier` (
   KEY `Panier_compte0_FK` (`mail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
---
--- Déchargement des données de la table `panier`
---
-
-INSERT INTO `panier` (`id`, `id_contenance`, `mail`, `qte`) VALUES
-('c01', 1, 'timote.anglade.45@gmail.com', 2),
-('f02', 1, 'timote.anglade.45@gmail.com', 1),
-('f02', 2, 'timote.anglade.45@gmail.com', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -232,8 +241,8 @@ INSERT INTO `panier` (`id`, `id_contenance`, `mail`, `qte`) VALUES
 DROP TABLE IF EXISTS `produit`;
 CREATE TABLE IF NOT EXISTS `produit` (
   `id` char(32) NOT NULL,
-  `libelle` varchar(255) NOT NULL,
-  `description` char(50) DEFAULT NULL,
+  `libelle` varchar(50) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
   `image` char(100) DEFAULT NULL,
   `dateMiseEnRayon` date NOT NULL,
   `id_categorie` char(32) NOT NULL,
@@ -248,27 +257,27 @@ CREATE TABLE IF NOT EXISTS `produit` (
 --
 
 INSERT INTO `produit` (`id`, `libelle`, `description`, `image`, `dateMiseEnRayon`, `id_categorie`, `id_marque`) VALUES
-('c01', 'Shampooing Douche BIO', 'Shampooing Douche au Thé Vert BIO', 'images/laino-shampooing-douche-au-the-vert-bio-200ml.png', '0000-00-00', 'CH', 1),
-('c02', 'Après shampooing', 'Fibres de lin baume après shampooing', 'images/klorane-fibres-de-lin-baume-apres-shampooing-150-ml.jpg', '0000-00-00', 'CH', 2),
-('c03', 'Shampooing orange', 'Kids 2in1 Shower & Shampoo Orange fruitée', 'images/weleda-kids-2in1-shower-shampoo-orange-fruitee-150-ml.jpg', '0000-00-00', 'CH', 3),
-('c04', 'Shampooing vanille', 'Kids 2in1 Shower & Shampoo vanille douce', 'images/weleda-kids-2in1-shower-shampoo-vanille-douce-150-ml.jpg', '0000-00-00', 'CH', 3),
-('c05', 'Shampooing sec', 'Shampooing sec à l\'extrait d\'ortie', 'images/klorane-shampooing-sec-a-l-extrait-d-ortie-spray-150ml.png', '0000-00-00', 'CH', 2),
-('c06', 'Mousse volume', 'Mousse volume intense', 'images/phytopulp-mousse-volume-intense-200ml.jpg', '0000-00-00', 'CH', 4),
-('c07', 'Shampooing nutritif', 'Shampooing nutritif', 'images/bio-beaute-by-nuxe-shampooing-nutritif-200ml.png', '0000-00-00', 'CH', 5),
-('f01', 'Contour yeux', 'Contour des Yeux Multi-Fonctions', 'images/nuxe-men-contour-des-yeux-multi-fonctions-15ml.png', '0000-00-00', 'FO', 5),
-('f02', 'Tisane en sachet', 'Tisane nature sommirel bio', 'images/tisane-romon-nature-sommirel-bio-sachet-20.jpg', '0000-00-00', 'FO', 15),
-('f03', 'Crème pansement', 'Cicaplast crème pansement', 'images/la-roche-posay-cicaplast-creme-pansement-40ml.jpg', '0000-00-00', 'FO', 10),
-('f04', 'Stabilisateur cheville', 'Sport stabilisateur pour cheville', 'images/futuro-sport-stabilisateur-pour-cheville-deluxe-attelle-cheville.png', '0000-00-00', 'FO', 8),
-('f05', 'Pèse-personne électronique', 'Pèse-personne électronique weegschaal', 'images/microlife-pese-personne-electronique-weegschaal-ws80.jpg', '0000-00-00', 'FO', 11),
-('f06', 'Miel Liquide', 'Miel de Thym Liquide', 'images/melapi-miel-thym-liquide-500g.jpg', '0000-00-00', 'FO', 12),
-('f07', 'Pollen', 'Meliflor Pollen 200g', 'images/melapi-pollen-250g.jpg', '0000-00-00', 'FO', 12),
-('p01', 'Spray solaire', 'Solaire Spray très haute protection', 'images/avene-solaire-spray-tres-haute-protection-spf50200ml.png', '0000-00-00', 'PS', 6),
-('p02', 'Crème solaire', 'Solaire Lait très haute Protection', 'images/mustela-solaire-lait-tres-haute-protection-spf50-100ml.jpg', '0000-00-00', 'PS', 13),
-('p03', 'Crème', 'Eryfotona aAK fluid', 'images/isdin-eryfotona-aak-fluid-100-50ml.jpg', '0000-00-00', 'PS', 9),
-('p04', 'Spray visage', 'Anthélios 50+ Brume Visage', 'images/la-roche-posay-anthelios-50-brume-visage-toucher-sec-75ml.png', '0000-00-00', 'PS', 10),
-('p05', 'Huile Capillaire', 'Sun Huile Lactée Capillaire Protectrice', 'images/nuxe-sun-huile-lactee-capillaire-protectrice-100ml.png', '0000-00-00', 'PS', 5),
-('p06', 'Stick lèvres', 'Stick lèvres SPF30', 'images/uriage-bariesun-stick-levres-spf30-4g.jpg', '0000-00-00', 'PS', 14),
-('p07', 'Crème cicatrisante', 'Cicabio creme SPF50+', 'images/bioderma-cicabio-creme-spf50-30ml.png', '0000-00-00', 'PS', 7);
+('c01', 'Shampooing Douche BIO', 'Shampooing Douche au Thé Vert BIO', 'images/laino-shampooing-douche-au-the-vert-bio-200ml.png', '0001-00-00', 'CH', 1),
+('c02', 'Après shampooing', 'Fibres de lin baume après shampooing', 'images/UPLOADED_klorane-fibres-de-lin-baume-apres-shampooing-150-ml.jpg', '0002-00-00', 'CH', 2),
+('c03', 'Shampooing orange', 'Kids 2in1 Shower & Shampoo Orange fruitée', 'images/weleda-kids-2in1-shower-shampoo-orange-fruitee-150-ml.jpg', '0003-00-00', 'CH', 3),
+('c04', 'Shampooing vanille', 'Kids 2in1 Shower & Shampoo vanille douce', 'images/weleda-kids-2in1-shower-shampoo-vanille-douce-150-ml.jpg', '0004-00-00', 'CH', 3),
+('c05', 'Shampooing sec', 'Shampooing sec à l\'extrait d\'ortie', 'images/klorane-shampooing-sec-a-l-extrait-d-ortie-spray-150ml.png', '0005-00-00', 'CH', 2),
+('c06', 'Mousse volume', 'Mousse volume intense', 'images/phytopulp-mousse-volume-intense-200ml.jpg', '0006-00-00', 'CH', 4),
+('c07', 'Shampooing nutritif', 'Shampooing nutritif', 'images/bio-beaute-by-nuxe-shampooing-nutritif-200ml.png', '0007-00-00', 'CH', 5),
+('f01', 'Contour yeux', 'Contour des Yeux Multi-Fonctions', 'images/nuxe-men-contour-des-yeux-multi-fonctions-15ml.png', '0008-00-00', 'FO', 5),
+('f02', 'Tisane en sachet', 'Tisane nature sommirel bio', 'images/tisane-romon-nature-sommirel-bio-sachet-20.jpg', '0009-00-00', 'FO', 15),
+('f03', 'Crème pansement', 'Cicaplast crème pansement', 'images/la-roche-posay-cicaplast-creme-pansement-40ml.jpg', '0010-00-00', 'FO', 10),
+('f04', 'Stabilisateur cheville', 'Sport stabilisateur pour cheville', 'images/futuro-sport-stabilisateur-pour-cheville-deluxe-attelle-cheville.png', '0011-00-00', 'FO', 8),
+('f05', 'Pèse-personne électronique', 'Pèse-personne électronique weegschaal', 'images/microlife-pese-personne-electronique-weegschaal-ws80.jpg', '0012-00-00', 'FO', 11),
+('f06', 'Miel Liquide', 'Miel de Thym Liquide', 'images/melapi-miel-thym-liquide-500g.jpg', '0013-00-00', 'FO', 12),
+('f07', 'Pollen', 'Meliflor Pollen 200g', 'images/melapi-pollen-250g.jpg', '0014-00-00', 'FO', 12),
+('p01', 'Spray solaire', 'Solaire Spray très haute protection', 'images/avene-solaire-spray-tres-haute-protection-spf50200ml.png', '0015-00-00', 'PS', 6),
+('p02', 'Crème solaire', 'Solaire Lait très haute Protection', 'images/mustela-solaire-lait-tres-haute-protection-spf50-100ml.jpg', '0016-00-00', 'PS', 13),
+('p03', 'Crème', 'Eryfotona aAK fluid', 'images/isdin-eryfotona-aak-fluid-100-50ml.jpg', '0017-00-00', 'PS', 9),
+('p04', 'Spray visage', 'Anthélios 50+ Brume Visage', 'images/la-roche-posay-anthelios-50-brume-visage-toucher-sec-75ml.png', '0018-00-00', 'PS', 10),
+('p05', 'Huile Capillaire', 'Sun Huile Lactée Capillaire Protectrice', 'images/nuxe-sun-huile-lactee-capillaire-protectrice-100ml.png', '0019-00-00', 'PS', 5),
+('p06', 'Stick lèvres', 'Stick lèvres SPF30', 'images/uriage-bariesun-stick-levres-spf30-4g.jpg', '0020-00-00', 'PS', 14),
+('p07', 'Crème cicatrisante', 'Cicabio creme SPF50+', 'images/bioderma-cicabio-creme-spf50-30ml.png', '0021-00-00', 'PS', 7);
 
 -- --------------------------------------------------------
 
@@ -303,7 +312,7 @@ INSERT INTO `produitcontenance` (`id`, `id_contenance`, `prix`, `qte`, `stock`, 
 ('c07', 1, 8, 200, 50, 1, 1),
 ('f01', 1, 12, 15, 50, 1, 1),
 ('f02', 1, 6, 20, 50, 1, 6),
-('f02', 2, 12, 40, 5, 0, 6),
+('f02', 2, 12, 40, 0, 0, 6),
 ('f03', 1, 11, 40, 50, 1, 1),
 ('f04', 1, 27, 1, 50, 1, 7),
 ('f05', 1, 63, 1, 50, 1, 7),
@@ -326,9 +335,18 @@ INSERT INTO `produitcontenance` (`id`, `id_contenance`, `prix`, `qte`, `stock`, 
 DROP TABLE IF EXISTS `promotion`;
 CREATE TABLE IF NOT EXISTS `promotion` (
   `id_reduc` float NOT NULL,
+  `reduction` float NOT NULL,
   `poidsReco` int(11) NOT NULL,
   PRIMARY KEY (`id_reduc`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `promotion`
+--
+
+INSERT INTO `promotion` (`id_reduc`, `reduction`, `poidsReco`) VALUES
+(1, 0.5, 3),
+(2, 0.9, 2);
 
 -- --------------------------------------------------------
 
@@ -344,6 +362,19 @@ CREATE TABLE IF NOT EXISTS `recommande` (
   KEY `recommande_produit0_FK` (`id_produit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+--
+-- Déchargement des données de la table `recommande`
+--
+
+INSERT INTO `recommande` (`id`, `id_produit`) VALUES
+('c01', 'c02'),
+('c01', 'c04'),
+('c01', 'c07'),
+('c01', 'f03'),
+('c01', 'f04'),
+('c01', 'p02'),
+('c04', 'c06');
+
 -- --------------------------------------------------------
 
 --
@@ -354,9 +385,23 @@ DROP TABLE IF EXISTS `sur`;
 CREATE TABLE IF NOT EXISTS `sur` (
   `id_reduc` float NOT NULL,
   `id` char(32) NOT NULL,
-  PRIMARY KEY (`id_reduc`,`id`),
-  KEY `sur_produit0_FK` (`id`)
+  `id_contenance` int(11) NOT NULL,
+  PRIMARY KEY (`id_reduc`,`id`,`id_contenance`),
+  KEY `sur_produit0_FK` (`id`),
+  KEY `sur_produitcontenance_fk` (`id`,`id_contenance`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `sur`
+--
+
+INSERT INTO `sur` (`id_reduc`, `id`, `id_contenance`) VALUES
+(1, 'c01', 1),
+(1, 'c02', 1),
+(1, 'f02', 1),
+(1, 'f02', 2),
+(1, 'p06', 1),
+(2, 'f04', 1);
 
 -- --------------------------------------------------------
 
@@ -400,20 +445,14 @@ ALTER TABLE `archivecontenir`
 -- Contraintes pour la table `avis`
 --
 ALTER TABLE `avis`
-  ADD CONSTRAINT `avis_compte_FK` FOREIGN KEY (`mail`) REFERENCES `compte` (`mail`);
+  ADD CONSTRAINT `avis_compte_fk` FOREIGN KEY (`mail`) REFERENCES `compte` (`mail`),
+  ADD CONSTRAINT `avis_produit_fk` FOREIGN KEY (`id`) REFERENCES `produit` (`id`);
 
 --
 -- Contraintes pour la table `commande`
 --
 ALTER TABLE `commande`
   ADD CONSTRAINT `commande_compte_FK` FOREIGN KEY (`mail`) REFERENCES `compte` (`mail`);
-
---
--- Contraintes pour la table `concerne`
---
-ALTER TABLE `concerne`
-  ADD CONSTRAINT `concerne_avis0_FK` FOREIGN KEY (`mail`,`idAvis`) REFERENCES `avis` (`mail`, `idAvis`),
-  ADD CONSTRAINT `concerne_produit_FK` FOREIGN KEY (`id`) REFERENCES `produit` (`id`);
 
 --
 -- Contraintes pour la table `contenir`
@@ -454,7 +493,7 @@ ALTER TABLE `recommande`
 -- Contraintes pour la table `sur`
 --
 ALTER TABLE `sur`
-  ADD CONSTRAINT `sur_produit0_FK` FOREIGN KEY (`id`) REFERENCES `produit` (`id`),
+  ADD CONSTRAINT `sur_produitcontenance_fk` FOREIGN KEY (`id`,`id_contenance`) REFERENCES `produitcontenance` (`id`, `id_contenance`),
   ADD CONSTRAINT `sur_promotion_FK` FOREIGN KEY (`id_reduc`) REFERENCES `promotion` (`id_reduc`);
 COMMIT;
 
